a17b1da4301aae1c0f91f59244558bd356adcedf
Anubhav <anubhavjaiswal2002@gmail.com>
2023-12-27 22:21:03 +0530

    successfully changed from java to kotlin 27-12-2023s

---
+++ app/src/main/java/com/example/resourcesapp/ui/earn/sub/NFTGeneratorFragment.kt
@@ -0,0 +1,47 @@
+package com.example.resourcesapp.ui.earn.sub
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.resourcesapp.R
+import com.example.resourcesapp.views.listingAdapter
+import com.example.resourcesapp.views.listingModel
+
+class NFTGeneratorFragment : Fragment() {
+    var lists: ArrayList<listingModel>? = null
+    var rv: RecyclerView? = null
+    var adapter: listingAdapter? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_n_f_t_generator, container, false)
+        rv = view.findViewById(R.id.recyclerView_NFTGenerator)
+        listings()
+        return view
+    }
+
+    fun listings() {
+        lists = ArrayList()
+        /*        sample here
+        lists.add(new listingModel("", "", ""));
+*/lists!!.add(listingModel("", "", ""))
+        lists!!.add(listingModel("", "", ""))
+        lists!!.add(listingModel("", "", ""))
+        adapter = listingAdapter(requireContext(), lists!!)
+        val layoutManager: RecyclerView.LayoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        rv!!.layoutManager = layoutManager
+        rv!!.adapter = adapter
+    }
+
+    companion object {
+        fun newInstance(): NFTGeneratorFragment {
+            return NFTGeneratorFragment()
+        }
+    }
+}
